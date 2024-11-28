5a668697558ab01bfbe1985748b9c9b25f758ea4
Entropire <158569522+Entropire@users.noreply.github.com>
2024-11-28 11:16:04 +0100

    jump fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,21 +1,24 @@
-using System;
+using StateMachine;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D Rb;
-    private bool IsGrounded;
-    private float JumpForceMultiplier;
+    private float initialY;
     
     [SerializeField] private float MaxSpeed;
+    
     [SerializeField] private float JumpForce;
+    [SerializeField] private float MaxJumpHeight;
+    
+    [SerializeField] private Transform GroundCheck;
     [SerializeField] private float GroundCheckRadius;
 
     void Start()
     {
         if (!TryGetComponent(out Rb))
         {
-            Console.Error.WriteLine("PlayerMovement component not found");
+            Debug.LogError("Rigidbody2D not found on PlayerMovement!");
         }
     }
 
@@ -36,34 +39,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKey(KeyCode.W) && IsGrounded && JumpForceMultiplier <= 1)
+        if (Input.GetKey(KeyCode.W) && IsGrounded())
         {
-            JumpForceMultiplier = JumpForceMultiplier + Time.deltaTime > 1 ? 1 : JumpForceMultiplier + Time.deltaTime; 
+            PlayerState.IsJumping = true;
+            initialY = transform.position.y; 
+        }
+
+        if (Input.GetKey(KeyCode.W) && PlayerState.IsJumping)
+        {
+            if (transform.position.y - initialY >= MaxJumpHeight)
+            {
+                PlayerState.IsJumping = false;
+                return;
+            }
+            
+            Rb.velocity = new Vector2(Rb.velocity.x, JumpForce);
         }
         
-        if (Input.GetKeyUp(KeyCode.W) && IsGrounded)
+        if (Input.GetKeyUp(KeyCode.Space) || Rb.velocity.y <= 0)
         {
-            Rb.velocity = new Vector2(Rb.velocity.x, JumpForce * JumpForceMultiplier);
-            JumpForceMultiplier = 0;
+            PlayerState.IsJumping  = false;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private bool IsGrounded()
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            IsGrounded = true;
-        }
+        return Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, LayerMask.GetMask("Ground"));
     }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            IsGrounded = false;
-        }
-    }
-}
+}   
 
 
 
