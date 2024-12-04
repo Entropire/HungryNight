34a47991054ffe9e006392a5e5209b931108d3ce
L0rdSquidy <144682503+L0rdSquidy@users.noreply.github.com>
2024-12-04 12:41:13 +0100

    spikeTrap Inplimentation

--- Assets/Scripts/HitPushback.cs
+++ Assets/Scripts/HitPushback.cs
@@ -3,16 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions.Must;
 
 public class HitPushback : MonoBehaviour
 {
     private Rigidbody2D rb;
     private bool ishitCooldown;
-    private float timeelepsed;
+    private int YhitCords = 4;
     public static event Action Gethit;
+    public Image Blackfade;
+    [SerializeField] private LayerMask Spiketrap;
+    [SerializeField] private Color BaseColor;
+    [SerializeField] private Color CurrentColor;
+    [SerializeField] private Color ToColor;
     [SerializeField] private float Hittime = 0.03f;
+    [SerializeField] private float timeelepsed;
+    [SerializeField] private float hitCooldowntime = 1f;
     void Start()
     {
+        Blackfade.color = BaseColor;
+        ToColor = BaseColor;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,10 +37,21 @@ public class HitPushback : MonoBehaviour
             {
                 Time.timeScale = 1;
             }
-            if (timeelepsed > 1f)
+            if (timeelepsed > hitCooldowntime)
             {
                 ishitCooldown = false;
+                timeelepsed = 0;
             }
+            
+        }   
+        if (Physics2D.CircleCast(transform.position, 2, Vector2.zero, 8f, Spiketrap))
+        {
+            Debug.Log("Ghello");
+        }
+        Blackfade.color = Color.Lerp(Blackfade.color, ToColor, 1f * Time.fixedDeltaTime);
+        if (Blackfade.color == Color.black)
+        {
+            ToColor =  BaseColor;
         }
     }
 
@@ -41,14 +63,26 @@ public class HitPushback : MonoBehaviour
             // Debug.Log(direction);
             if (!ishitCooldown)
             {
+                Debug.Log(direction.y);
+                if(direction.y > 0 && other.GetComponent<SpikeFall>() == null)
+                {
+                    YhitCords = 16;
+                    ToColor = Color.black;
+                }else
+                {
+                    YhitCords = 7;
+                    ToColor = BaseColor;
+                }
+
                 Gethit?.Invoke();
+
                 if (direction.x > 0)
-            {
-                rb.AddForce(new Vector2(4, 4f), ForceMode2D.Impulse);
-            }
+                {
+                    rb.AddForce(new Vector2(4, YhitCords), ForceMode2D.Impulse);
+                }
             else 
             {
-                rb.AddForce(new Vector2(-4, 4f), ForceMode2D.Impulse);
+                rb.AddForce(new Vector2(-4, YhitCords), ForceMode2D.Impulse);
             }
             Time.timeScale = 0.1f;
             }
