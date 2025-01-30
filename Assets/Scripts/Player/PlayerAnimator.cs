ea029c5b5aea8f138fb3e4fd0c1be77b940474c8
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-01-30 11:50:28 +0100

    cleaned more and updated the playeranimcontroller

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,56 +1,53 @@
 ﻿using UnityEngine;
 
-namespace StateMachine
+[RequireComponent(typeof(PlayerState))]
+public class PlayerAnimator : MonoBehaviour
 {
-    [RequireComponent(typeof(PlayerState))]
-    public class PlayerAnimator : MonoBehaviour
+  Animator animator;
+  PlayerState playerState;
+
+  private void Start()
+  {
+    animator = GetComponent<Animator>();
+    playerState = GetComponent<PlayerState>();
+  }
+
+  private void Update()
+  {
+    ResetTriggers();
+    if (playerState.IsWalking && !playerState.IsJumping)
     {
-        Animator animator;
-        PlayerState playerState;
-
-        private void Start()
-        {
-            animator = GetComponent<Animator>();
-            playerState = GetComponent<PlayerState>();
-        }
-
-        private void Update()
-        {
-            ResetTriggers();
-            if (playerState.IsWalking && !playerState.IsJumping)
-            {
-                animator.SetTrigger("IsWalking");
-            }
-            else if (playerState.IsJumping && !playerState.IsFalling)
-            {
-                animator.SetTrigger("IsJumping");
-            }
-            else if (playerState.IsFalling  && !playerState.IsJumping)
-            {
-                animator.SetTrigger("IsFalling");
-            }
-            else if (playerState.IsAttacking)
-            {
-                animator.SetTrigger("IsAttacking");
-            }
-            else if (playerState.IsHit)
-            {
-                animator.SetTrigger("IsHit");
-            }
-            else
-            {
-                animator.SetTrigger("IsIdle");
-            }
-        }
-
-        private void ResetTriggers()
-        {
-            animator.ResetTrigger("IsJumping");
-            animator.ResetTrigger("IsFalling");
-            animator.ResetTrigger("IsWalking");
-            animator.ResetTrigger("IsAttacking");
-            animator.ResetTrigger("IsHit");
-            animator.ResetTrigger("IsIdle");
-        }
+      animator.SetTrigger("IsWalking");
     }
+    else if (playerState.IsJumping && !playerState.IsFalling)
+    {
+      animator.SetTrigger("IsJumping");
+    }
+    else if (playerState.IsFalling && !playerState.IsJumping)
+    {
+      animator.SetTrigger("IsFalling");
+    }
+    else if (playerState.IsAttacking)
+    {
+      animator.SetTrigger("IsAttacking");
+    }
+    else if (playerState.IsHit)
+    {
+      animator.SetTrigger("IsHit");
+    }
+    else
+    {
+      animator.SetTrigger("IsIdle");
+    }
+  }
+
+  private void ResetTriggers()
+  {
+    animator.ResetTrigger("IsJumping");
+    animator.ResetTrigger("IsFalling");
+    animator.ResetTrigger("IsWalking");
+    animator.ResetTrigger("IsAttacking");
+    animator.ResetTrigger("IsHit");
+    animator.ResetTrigger("IsIdle");
+  }
 }
